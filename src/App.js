23db4c23cff8389a0a0feadf5dbda6b497aa7a82
cpscott1 <thacam18gmail.com>
2021-03-15 20:49:10 -0500

    added header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useContext, useEffect } from 'react'
-import { Box, Icon, Grid, Image, Text, Badge } from '@chakra-ui/react'
+import { Box, Icon, Grid, Image, Text, Badge, Button } from '@chakra-ui/react'
 import { Link, Switch, Route } from 'react-router-dom'
 import { ShopContext } from './context/shopContext'
 import { MdShoppingBasket, BsSearch } from 'react-icons'
+import './App.css'
 import './navbar.css'
-import topimage from './images/top-image.jpg'
-import bottomimage from './images/top-image2.jpg'
+import topimage from './images/top-image5.jpg'
+import bottomimage from './images/top-image3.jpg'
 function App() {
 
   const { fetchAllCollections, fetchCollection, products } = useContext(ShopContext)
@@ -19,34 +20,40 @@ function App() {
       <Grid templateColumns="repeat(3, 1fr)" textAlign="center">
         <Box></Box>
         <Box>
-          <Text fontSize="4xl" marginRight="2rem">Devvy</Text>
+          <Text className="logo" fontSize="4xl" marginRight="2rem">Devvy</Text>
           <Link to="#"><a className="links">NIKE</a></Link>
           <Link to="#"><a className="links">JORDAN</a></Link>
           <Link to="#"><a className="links">YEEZY</a></Link>
         </Box>
         <Box><Text>Search</Text></Box>
       </Grid>
-      <Box w="100%" position="relative" h="27vw">
-        <Image src={topimage} h="25vw" m="auto" objectFit="contain" />
-        <Text
-          position="absolute"
-          color="black"
-          left="25vw"
-          bottom="10vh"
-          fontWeight="bold"
-          fontSize="4rem"
-        >Air Jordan</Text>
+      <Box className="image-container" w="100%" h="30rem" position="relative">
+        <Image src={topimage}  m="auto" objectFit="contain" />
+        <Box className="border" textAlign="center"
+        position="absolute"
+        color="white"
+        left="27%"
+        bottom="10vh"
+        fontWeight="bold"
+        fontSize="4rem"
+        >
+          <Text className="hero-text">Air Jordan</Text>
+          <Button className="hero-button">Shop Now</Button>
+        </Box>
       </Box>
-      <Box w="100%" position="relative" h="27vw">
-        <Image src={bottomimage} h="25vw" m="auto" objectFit="contain" />
-        <Text
-          position="absolute"
-          left="25vw"
-          bottom="45vh"
-          color="white"
-          fontWeight="bold"
-          fontSize="4rem"
-        >Yeezy Season</Text>
+      <Box w="100%" position="relative" h="30rem">
+        <Image src={bottomimage}  m="auto" objectFit="contain" />
+        <Box className="border" textAlign="center"
+        position="absolute"
+        left="27%"
+        bottom="60%"
+        color="white"
+        fontWeight="bold"
+        fontSize="4rem"
+        >
+        <Text className="hero-text">Yeezy Season</Text>
+        <Button className="hero-button">Shop Now</Button>
+        </Box>
       </Box>
       <Box>
 
